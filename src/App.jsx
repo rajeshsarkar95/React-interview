7830bf6441b5a1bb21f56c35c65a2ac45e9d1ef7
prabhakarsarkar <prabhakar19@navgurukul.org>
2025-01-08 11:35:16 +0530

    serchfilter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import PasswordValidation from "./componets/passwordvalidation/password"
 import TodoList from "./componets/todolist/TodoList"
+import SearchFilter from "./componets/serachfilter/serchfilter"
 
 function App() {
   return (
     <div>
      {/* <PasswordValidation/> */}
-     <TodoList/>
+     {/* <TodoList/> */}
+     <SearchFilter/>
     </div>
   )
 }
---
+++ src/componets/serachfilter/serchfilter.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function serchfilter() {
+    const [query,setQuery] = useState("");
+    const name = ["apple","banana","oranges","papays"];
+    const namefilter  = name.filter((name)=>
+        name.toLowerCase().includes(query.toLowerCase())
+    )
+  return (
+    <div>
+      <input 
+      type="text"
+      value={query}
+      placeholder="Serch names"
+      onChange={(e) =>setQuery(e.target.value)}
+      />
+      <div>
+        {namefilter.length > 0 ? (
+          <ul>
+            {namefilter.map((name,index)=>(
+                <li key={index}>{name}</li>
+            ))}
+          </ul>
+        ):(
+         <p>name is not found  </p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default serchfilter
